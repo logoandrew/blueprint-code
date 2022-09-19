769df97b9dbb78beb50591ce3ced65e8d40afd99
Andrew Newhouse <logoandrew@gmail.com>
2022-09-19 17:16:16 -0400

    fix links for gh-pages

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,8 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: 'src',
+      includes: '_includes',
+      data: '_data',
       output: 'docs',
     },
     pathPrefix: '/blueprint-code/',
