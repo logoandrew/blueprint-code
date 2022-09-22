b6a1cddbe814e07884cc8543ef63b5faaef2d32c
Andrew Newhouse <logoandrew@gmail.com>
2022-09-22 12:56:26 -0400

    refactor panel variables

--- src/_data/placeholder.js
+++ src/_data/placeholder.js
@@ -1,11 +1,9 @@
+var getlorem = require('getlorem');
+
 module.exports = function () {
   return {
     imgURL: function (w, h, r) {
-      if (r !== undefined) {
-        r = r;
-      } else {
-        r = 1;
-      }
+      var r = r !== undefined ? r : 1;
       return (
         'https://picsum.photos/' +
         w +
@@ -15,6 +13,32 @@ module.exports = function () {
         r +
         ''
       );
+    },
+    paragraphs: function (c, p) {
+      var output = ' ';
+      var x;
+      var y = c;
+      var low = c / p / 1.5;
+      var high = (c / p) * 1.5;
+      for (let i = 1; i < p; i++) {
+        x = Math.floor(Math.random() * y) + 1;
+        x = x < low ? low : x;
+        x = x > high ? high : x;
+        y -= x;
+        if (y <= 20) {
+          x += y;
+          i = p;
+          y = 0;
+        }
+        output += '<p>' + getlorem.bytes(x) + '</p>';
+      }
+      if (y > 0) {
+        output += '<p>' + getlorem.bytes(y) + '</p>';
+      }
+      return output;
+    },
+    char: function (c) {
+      return getlorem.bytes(c);
     },
   };
 };
